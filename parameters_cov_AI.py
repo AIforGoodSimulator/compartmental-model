e0bddddec6304520ec4e0d82d63aa4bd3c331be5
Billy Zhao <billyzhaoyh@gmail.com>
2020-04-16 19:08:27 +0100

    to make file reading working across the system

--- parameters_cov_AI.py
+++ parameters_cov_AI.py
@@ -7,9 +7,10 @@ import numpy as np
 import pandas as pd
 from scipy.stats import gamma, norm
 from math import ceil
+import os
+cwd = os.getcwd()
 
-
-raw_data = pd.read_csv('AI_for_good/AI-for-good/camp_params.csv')
+raw_data = pd.read_csv(os.path.join(cwd,'camp_params.csv'))
 
 def preparePopulationFrame(camp_name):
     
@@ -41,7 +42,7 @@ example_population_frame, example_population = preparePopulationFrame('Camp_1')
 
 #------------------------------------------------------------
 # disease params
-parameter_csv = pd.read_csv('AI_for_good/AI-for-good/parameters.csv')
+parameter_csv = pd.read_csv(os.path.join(cwd,'parameters.csv'))
 model_params = parameter_csv[parameter_csv['Type']=='Model Parameter']
 model_params = model_params.loc[:,['Name','Value']]
 # print()
