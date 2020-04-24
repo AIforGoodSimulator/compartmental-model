5cccfcd65342f150803517829a431dc4acbca4a6
nt409 <nt409@cam.ac.uk>
2020-04-24 15:30:31 +0100

    tidying

--- Scripts/config.py
+++ Scripts/config.py
@@ -1,22 +1,19 @@
 from initialise_parameters import preparePopulationFrame
 import numpy as np
 
-control_type = 'No control'
+# camp
 camp = 'Camp_2'
-timings = [10,100] # control timings
-taken_offsite_rate = 100 # people per day
-remove_high_risk = 100 # people per day
 population_frame, population = preparePopulationFrame(camp)
 
+# control timings
+timings = [10,100]
 
+# choice of no control, or better hygeine
+control_type = 'No control'
 
+# move symptomatic cases off site
+taken_offsite_rate = 100 # people per day
 
-##
-# create dict for removal
-# Ages = np.asarray(population_frame.Age)
-# remove_people = [remove_high_risk if i ==len(Ages)-1 else 0 for i in range(len(Ages))]
-
-# remove_people = {}
-# for i, age in enumerate(Ages):
-#     remove_people[age] = removal[i]
+# move uninfected high risk people off site
+remove_high_risk = 100 # people per day
 
