b5b43d523ce553ef7110bacf9259ff21868d8b4c
Billy Zhao <billyzhaoyh@gmail.com>
2020-05-06 00:50:24 +0100

    removing artefacts and creating test folder

--- Tests/test.py
+++ Tests/test.py
@@ -5,4 +5,10 @@
 #tracking population total over the course of simulation to see if the total numbers are right
 #tracking the population within each age compartment to see if the number remain constant with the time step passing to observe is there is any abnormal flow patter
 
-import Scripts
+# Bring your packages onto the path
+import sys, os
+sys.path.append(os.path.abspath(os.path.join('..', 'Scripts')))
+
+# Now do your import
+from Scripts import *
+
